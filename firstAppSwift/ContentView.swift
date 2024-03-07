e1710cc19be05c11297a02613bfdb49dea9c39b8
karimamaaoui <maaouika07@gmail.com>
2024-03-07 11:12:52 +0100

    Initial Commit

---
+++ firstAppSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstAppSwift
+//
+//  Created by karimamaaoui on 3/7/24.
+//  Copyright © 2024 karimamaaoui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
